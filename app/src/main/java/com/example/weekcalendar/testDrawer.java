2d221dcd237824dd7e04276df908d286c6424da8
jlxw48 <jlxw48@gmail.com>
2020-05-30 11:01:14 +0800

    working navigation pane

--- app/src/main/java/com/example/weekcalendar/testDrawer.java
+++ app/src/main/java/com/example/weekcalendar/testDrawer.java
@@ -7,6 +7,7 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.navigation.NavigationView;
@@ -22,42 +23,41 @@ public class testDrawer extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_drawer);
 
+        Toolbar toolbar = findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar);
+
         dl = findViewById(R.id.drawer_layout);
-        t = new ActionBarDrawerToggle(this, dl,R.string.Open, R.string.Close);
+        t = new ActionBarDrawerToggle(this, dl, toolbar, R.string.Open, R.string.Close);
 
         dl.addDrawerListener(t);
         t.syncState();
 
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
         nv = findViewById(R.id.nv);
-        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int id = item.getItemId();
-                switch(id)
-                {
-                    case R.id.account:
-                        Toast.makeText(testDrawer.this, "My Account",Toast.LENGTH_SHORT).show();break;
-                    case R.id.settings:
-                        Toast.makeText(testDrawer.this, "Settings",Toast.LENGTH_SHORT).show();break;
-                    case R.id.mycart:
-                        Toast.makeText(testDrawer.this, "My Cart",Toast.LENGTH_SHORT).show();break;
-                    default:
-                        return true;
-                }
-
-
-                return true;
-
+        nv.setNavigationItemSelectedListener(item -> {
+            int id = item.getItemId();
+            switch(id)
+            {
+                case R.id.account:
+                    Toast.makeText(testDrawer.this, "My Account",Toast.LENGTH_SHORT).show();break;
+                case R.id.settings:
+                    Toast.makeText(testDrawer.this, "Settings",Toast.LENGTH_SHORT).show();break;
+                case R.id.mycart:
+                    Toast.makeText(testDrawer.this, "My Cart",Toast.LENGTH_SHORT).show();break;
+                default:
+                    return true;
             }
+            return true;
+
         });
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
         if(t.onOptionsItemSelected(item))
             return true;
-
         return super.onOptionsItemSelected(item);
     }
 }
