8e5101dbfcecef2f8808b788bd57d68c6b70f6eb
jlxw48 <jlxw48@gmail.com>
2020-06-20 11:55:13 +0800

    update event functionality

--- app/src/main/java/com/example/weekcalendar/ActivityCreateEventPage.java
+++ app/src/main/java/com/example/weekcalendar/ActivityCreateEventPage.java
@@ -11,8 +11,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
@@ -23,9 +26,13 @@ public class ActivityCreateEventPage extends AppCompatActivity implements MyDate
 
     // XML variables
     private EditText title;
+    private TextInputLayout selectStartDateLayout;
     private Button selectStartDate;
+    private TextInputLayout selectEndDateLayout;
     private Button selectEndDate;
+    private TextInputLayout selectStartTimeLayout;
     private Button selectStartTime;
+    private TextInputLayout selectEndTimeLayout;
     private Button selectEndTime;
     private Button createEvent;
     private EditText todo1;
@@ -36,6 +43,8 @@ public class ActivityCreateEventPage extends AppCompatActivity implements MyDate
     private String userID;
     private CollectionReference cEvents;
     private CollectionReference cToDo;
+
+    private CustomEvent event;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +60,11 @@ public class ActivityCreateEventPage extends AppCompatActivity implements MyDate
 
         // Links to XML
         this.title = findViewById(R.id.insert_event_name);
+
+        this.selectStartDateLayout = findViewById(R.id.selectStartDateLayout);
+        this.selectEndDateLayout = findViewById(R.id.selectEndDateLayout);
+//        this.selectStartTimeLayout = findViewById(R.id.selectStartDateLayout);
+//        this.selectEndTimeLayout = findViewById(R.id.selectStartDateLayout);
 
         this.selectStartDate = findViewById(R.id.select_start_date);
         this.selectStartDate.setOnClickListener(v -> openSelectDateDialog(this.selectStartDate));
@@ -79,13 +93,15 @@ public class ActivityCreateEventPage extends AppCompatActivity implements MyDate
         });
 
         Intent i = getIntent();
-        CustomEvent event = i.getParcelableExtra("event to edit");
+        this.event = i.getParcelableExtra("event to edit");
         if (event != null) {
             this.title.setText(event.getTitle());
             this.selectStartDate.setText(HelperMethods.formatDateForView(event.getStartDate()));
             this.selectEndDate.setText(HelperMethods.formatDateForView(event.getEndDate()));
             this.selectStartTime.setText(HelperMethods.formatTimeTo12H(event.getStartTime()));
             this.selectEndTime.setText(HelperMethods.formatTimeTo12H(event.getEndTime()));
+            this.createEvent.setText("Update Event");
+            this.createEvent.setOnClickListener(v -> updateEvent());
         }
     }
 
@@ -93,59 +109,107 @@ public class ActivityCreateEventPage extends AppCompatActivity implements MyDate
     private boolean checkFields() {
         String eventTitle = this.title.getText().toString();
         if (eventTitle.equals("")) {
-            Toast.makeText(this, "Please insert event title!", Toast.LENGTH_SHORT).show();
-        } else if (selectStartDate.getText().toString().equals("Select Start Date")) {
-            Toast.makeText(this, "Please choose a start date!", Toast.LENGTH_SHORT).show();
-        } else if (selectEndDate.getText().toString().equals("Select End Date")) {
-            Toast.makeText(this, "Please choose an end date!", Toast.LENGTH_SHORT).show();
-        } else if (selectStartTime.getText().toString().equals("Select Start Time")) {
-            Toast.makeText(this, "Please choose a start time!", Toast.LENGTH_SHORT).show();
-        } else if (selectEndTime.getText().toString().equals("Select End Time")) {
-            Toast.makeText(this, "Please choose an end time!", Toast.LENGTH_SHORT).show();
+            this.title.setError("Please insert event title!");
+        } else if (this.selectStartDate.getText().toString().equals("Select Start Date")) {
+            this.selectStartDateLayout.setError("Please choose a start date!");
+            this.selectStartDate.setError("Please choose a start date!");
+        } else if (this.selectEndDate.getText().toString().equals("Select End Date")) {
+            this.selectEndDateLayout.setError("Please choose an end date!");
+            this.selectEndDate.setError("Please choose an end date!");
+        } else if (this.selectStartTime.getText().toString().equals("Select Start Time")) {
+//            this.selectStartDateLayout.setError("Please choose a start date!");
+            this.selectStartTime.setError("Please choose a start time!");
+        } else if (this.selectEndTime.getText().toString().equals("Select End Time")) {
+//            this.selectStartDateLayout.setError("Please choose a start date!");
+            this.selectEndTime.setError("Please choose an end time!");
         } else {
             return true;
         }
         return false;
     }
 
+    private Map<String, Object> getEventDetails() {
+        String eventTitle = ((EditText) findViewById(R.id.insert_event_name)).getText().toString();
+        String startDate = HelperMethods.formatDateForFirebase(this.selectStartDate.getText().toString());
+        String endDate = HelperMethods.formatDateForFirebase(this.selectEndDate.getText().toString());
+        String startTime = HelperMethods.formatTimeTo24H(this.selectStartTime.getText().toString());
+        String endTime = HelperMethods.formatTimeTo24H(this.selectEndTime.getText().toString());
+
+        Map<String, Object> eventDetails = new HashMap<>();
+        eventDetails.put("userID", this.userID);
+        eventDetails.put("eventTitle", eventTitle);
+        eventDetails.put("startDate", startDate);
+        eventDetails.put("endDate", endDate);
+        eventDetails.put("startTime", startTime);
+        eventDetails.put("endTime", endTime);
+
+        return eventDetails;
+    }
+
+    private Map<String, Object> getToDoDetails() {
+        String toDo = this.todo1.getText().toString();
+
+        if (!toDo.equals("")) {
+            Map<String, Object> toDoDetails = new HashMap<>();
+            String startDate = HelperMethods.formatDateForFirebase(this.selectStartDate.getText().toString());
+            toDoDetails.put("userID", this.userID);
+            toDoDetails.put("date", startDate);
+            toDoDetails.put("title", toDo);
+            return toDoDetails;
+        } else {
+            return null;
+        }
+    }
+
+    private void updateEvent() {
+        if (checkFields()) {
+            DocumentReference thisEventDoc = this.cEvents.document(this.event.getId());
+//            DocumentReference thisToDoDoc = this.cToDo.document(this.event.getId());
+
+            Map<String, Object> eventDetails = getEventDetails();
+//            Map<String, Object> toDoDetails = getToDoDetails();
+
+            thisEventDoc.update(eventDetails)
+                    .addOnSuccessListener(docRef -> {
+                        // need to check if event originally had to do items
+                        // if true, then we need to update this based on the existing to do's document ID
+                        // if false, we need to create new document
+//                        if (toDoDetails != null) {
+//                            DocumentReference toDoDocRef = cToDo.document();
+//                            toDoDetails.put("eventID", this.event.getId());
+//                            toDoDetails.put("todoID", toDoDocRef.getId());
+//                            toDoDocRef.set(toDoDetails)
+//                                    .addOnSuccessListener(docRef2 -> Log.d(TAG, "DocumentSnapshot successfully written!"))
+//                                    .addOnFailureListener(e -> Log.w(TAG, "Error writing document", e));
+//                        }
+                        Log.d(TAG, "DocumentSnapshot successfully written!");
+                    })
+                    .addOnFailureListener(e -> Log.w(TAG, "Error writing document", e));
+
+            Intent i = new Intent(this, ActivityUpcomingPage.class);
+            startActivity(i);
+        }
+    }
+
     // need to implement multi day here
     private void createEvent() {
         if (checkFields()) {
-            String eventTitle = ((EditText) findViewById(R.id.insert_event_name)).getText().toString();
-            String toDo = this.todo1.getText().toString();
-            String startDate = HelperMethods.formatDateForFirebase(this.selectStartDate.getText().toString());
-            String endDate = HelperMethods.formatDateForFirebase(this.selectEndDate.getText().toString());
-            String startTime = HelperMethods.formatTimeTo24H(this.selectStartTime.getText().toString());
-            String endTime = HelperMethods.formatTimeTo24H(this.selectEndTime.getText().toString());
-
-            Map<String, Object> eventDetails = new HashMap<>();
-            eventDetails.put("userID", this.userID);
-            eventDetails.put("eventTitle", eventTitle);
-            eventDetails.put("startDate", startDate);
-            eventDetails.put("endDate", endDate);
-            eventDetails.put("startTime", startTime);
-            eventDetails.put("endTime", endTime);
-
-            Map<String, Object> toDoDetails = new HashMap<>();
-            if (!toDo.equals("")) {
-                toDoDetails.put("userID", this.userID);
-                toDoDetails.put("date", startDate);
-                toDoDetails.put("title", toDo);
-            }
+            Map<String, Object> eventDetails = getEventDetails();
+            Map<String, Object> toDoDetails = getToDoDetails();
 
             cEvents.add(eventDetails)
                     .addOnSuccessListener(docRef -> {
-                        if (!toDo.equals("")) {
+                        if (toDoDetails != null) {
+                            DocumentReference toDoDocRef = cToDo.document();
                             toDoDetails.put("eventID", docRef.getId());
-                            cToDo.add(toDoDetails)
+                            toDoDetails.put("todoID", toDoDocRef.getId());
+                            toDoDocRef.set(toDoDetails)
                                     .addOnSuccessListener(docRef2 -> Log.d(TAG, "DocumentSnapshot successfully written!"))
                                     .addOnFailureListener(e -> Log.w(TAG, "Error writing document", e));
                         }
                         Log.d(TAG, "DocumentSnapshot successfully written!");
                     })
                     .addOnFailureListener(e -> Log.w(TAG, "Error writing document", e));
-
-
 
             Intent i = new Intent(this, ActivityUpcomingPage.class);
             startActivity(i);
