ca7903e7ff5078a0ecd2c0db1e4a5903141190b0
Foo Say Han <fsayhan123@gmail.com>
2020-07-08 19:55:19 +0800

    Dynamic Link Dialog inflate layout

--- app/src/main/java/com/example/weekcalendar/helperclasses/Dialog.java
+++ app/src/main/java/com/example/weekcalendar/helperclasses/Dialog.java
@@ -2,15 +2,20 @@ package com.example.weekcalendar.helperclasses;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatDialogFragment;
 
+import com.example.weekcalendar.R;
+
 public class Dialog extends AppCompatDialogFragment {
     private String url;
-
+    private EditText email;
     public Dialog(String url) {
         this.url = url;
     }
@@ -18,9 +23,10 @@ public class Dialog extends AppCompatDialogFragment {
     @Override
     public android.app.Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-
-        builder.setTitle("Invite Others")
-                .setMessage(this.url)
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.dialog_invite, null);
+        builder.setView(view)
+                .setTitle("Invite Others")
                 .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
@@ -32,6 +38,7 @@ public class Dialog extends AppCompatDialogFragment {
                     public void onClick(DialogInterface dialog, int which) {
                     }
                 });
+        email = view.findViewById(R.id.edit_invite);
         return builder.create();
     }
 }
